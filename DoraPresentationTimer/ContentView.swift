405e63e48c7e096c3dadd2cbede5fae89b4e7afe
SAKI HIRAGUCHI <totokit4_saki@hey.jp>
2022-11-06 00:27:10 +0900

    :+1: create proj

---
+++ DoraPresentationTimer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DoraPresentationTimer
+//
+//  Created by totokit4_saki on 2022/11/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DoraPresentationTimer/DoraPresentationTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoraPresentationTimerApp.swift
+//  DoraPresentationTimer
+//
+//  Created by totokit4_saki on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct DoraPresentationTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
