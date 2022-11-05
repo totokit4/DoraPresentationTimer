fa15b52d038bc1d260e9a0cd05945493c14cbbc2
SAKI HIRAGUCHI <totokit4_saki@hey.jp>
2022-11-06 01:11:15 +0900

    :+1: 画面作成
    - 残時間表示用のラベルを作る
    - スタートボタンを作る
    - 停止ボタンを作る
    - リセットボタンを作る

--- DoraPresentationTimer/ContentView.swift
+++ DoraPresentationTimer/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var RemainingTimeText = "XX:XX"
+    @State var isStartable = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(RemainingTimeText)
+                .font(.largeTitle)
+                .padding(.bottom)
+            
+            HStack {
+                Button(action: {
+                    self.RemainingTimeText = "Start !"
+                    self.isStartable.toggle()
+                    // TODO: タイマー開始
+                }){
+                    Text("Start")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                }
+                .disabled(!isStartable)
+                .padding(.all)
+                .background(isStartable ? Color.orange : Color(UIColor.lightGray))
+                
+                Button(action: {
+                    self.RemainingTimeText = "Stop !"
+                    self.isStartable.toggle()
+                    // TODO: タイマー停止
+                }){
+                    Text("Pause")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                }
+                .disabled(isStartable)
+                .padding(.all)
+                .background(isStartable ? Color(UIColor.lightGray) : Color.orange)
+                
+                Button(action: {
+                    self.RemainingTimeText = "XX:XX"
+                    self.isStartable = true
+                    // TODO: タイマーリセット
+                }){
+                    Text("Reset")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                }
+                .padding(.all)
+                .background(Color.orange)
+            }
         }
-        .padding()
     }
 }
 
