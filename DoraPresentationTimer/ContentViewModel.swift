c7398a7fca7e9ac1748d28027c1ac8126da58115
totokit4 <totokit4_saki@st.inc>
2022-11-24 20:42:30 +0900

    :+1: SoundPlayModel追加

--- DoraPresentationTimer/ContentViewModel.swift
+++ DoraPresentationTimer/ContentViewModel.swift
@@ -6,16 +6,13 @@
 //
 
 import Foundation
-import AVFoundation
 import Combine
-import UIKit
 
 final class ContentViewModel: ObservableObject {
     @Published private(set) var count = 0
     @Published private(set) var isTimerRunning = false
     
     private var cancellable: AnyCancellable?
-    private var player: AVAudioPlayer?
     private var initCount = 0
     
     func startTimer(second: Int) {
@@ -49,15 +46,5 @@ final class ContentViewModel: ObservableObject {
     func resetCount() {
         stopTimer()
         count = initCount
-    }
-    
-    private func playSound(){
-        guard let soundFile = NSDataAsset(name: "Dora") else { return }
-        do {
-            player = try AVAudioPlayer(data:soundFile.data, fileTypeHint:"mp3")
-            player?.play()
-        } catch {
-            print("error")
-        }
     }
 }
